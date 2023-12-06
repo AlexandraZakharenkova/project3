c359b77a58c6eb074b8080e75a3ed2f04bda7730
AlexandraZakharenkova <151626157+AlexandraZakharenkova@users.noreply.github.com>
2023-12-06 13:52:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from docx_functions import modify_docx
+# Импортируем необходимые библиотеки и модули
+import os
+from docx import Document
 
-# Применяем функцию к каждому документу
-documents = ["doc1.docx", "doc2.docx", "doc3.docx", "doc4.docx", "doc5.docx"]
+# Импортируем функции из второго файла
+from docx_functions import change_font_and_spacing
 
-for doc_path in documents:
-    modify_docx(doc_path)
+# Указываем папку с файлами
+folder = 'путь_к_папке_с_файлами'
+
+# Указываем параметры для изменения
+font_name = 'times new roman'
+font_size = 14
+line_spacing = 1.5
+
+# Проходимся по всем файлам в папке
+for filename in os.listdir(folder):
+    if filename.endswith('.docx'):
+        file_path = os.path.join(folder, filename)
+
+        # Вызываем функцию для изменения параметров шрифта и межстрочного интервала
+        change_font_and_spacing(file_path, font_name, font_size, line_spacing)
+
+        print(f'документ \"{filename}\" успешно изменен.')
